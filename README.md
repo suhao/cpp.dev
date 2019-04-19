efb3165779389dd48ea837d04dbe949fb8a59448
suhao <638781@qq.com>
2019-04-19 17:13:46 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# test
+Project for test.
---
+++ test/test.cpp
@@ -0,0 +1,29 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2017 The Authors of ANT(http://ant.sh). All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#if defined(COMPILER_MSVC)
+// We usually use the _CrtDumpMemoryLeaks() with the DEBUGER and CRT library to
+// check a memory leak.
+#if defined(_DEBUG) && _MSC_VER > 1000 // VC++ DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#define  DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#if defined(DEBUG_NEW)
+#define new DEBUG_NEW
+#endif // DEBUG_NEW
+#endif // VC++ DEBUG
+#endif // defined(COMPILER_MSVC)
+
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n"; 
+}
